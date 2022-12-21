2bac3c0253f2549d59f94d053344c7b9b658d434
Hamad <hamad.coder@gmail.com>
2022-12-21 16:49:16 +0500

    [Omni Books Project Clean Code]

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
 // alert('Hello');
+document.body.className = 'hidden';
+window.addEventListener('DOMContentLoaded', function () {
+
+    function doStuff (callback) {
+        // do all app scripts here...
+        callback();
+    }
+
+    doStuff(function () {
+        document.body.className = 'visible';
+    });
+});
